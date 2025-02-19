982301d54afd138914a2e2c09662ae2eab65e255
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-19 15:28:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+document.addEventListener("DOMContentLoaded",()=>{
+	const images=document.querySelectorAll(".image");
+	let draggedElement=null;
+	images.forEach(image=>{
+		image.addEventListener("dragstart",(event)=>{
+			draggedElement=event.target;
+			event.target.classList.add("selected");
+		});
+
+		image.addEventListener("dragend",(event)=>{
+			event.target.classList.remove("selected");
+		})
+		image.addEventListener("dragover",(event)=>{
+			event.preventDefault();
+		});
+
+		image.addEventListener("drop",(event)=>{
+			event.preventDefault();
+			if(draggedElement&&draggedElement!==event.target){
+				 let tempBg = window.getComputedStyle(draggedElement).backgroundImage;
+                draggedElement.style.backgroundImage = window.getComputedStyle(event.target).backgroundImage;
+                event.target.style.backgroundImage = tempBg;
+			}
+		});
+		
+	});
+});
